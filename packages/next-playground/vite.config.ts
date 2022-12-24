afb65af280f0e12932cf477c39e83c90261356af
Rahul Tarak <cryogenicplanet@gmail.com>
2022-12-24 09:57:39 +0530

    Might be able to remove vite

--- packages/next-playground/vite.config.ts
+++ packages/next-playground/vite.config.ts
@@ -18,13 +18,14 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['react', 'node:fs/promises', 'node:fs'],
+      external: ['react', 'node:fs/promises', 'node:fs', 'next', 'leva'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
           react: 'react',
-          fs: 'fs'
+          fs: 'fs',
+          next: 'next'
         }
       }
     }
